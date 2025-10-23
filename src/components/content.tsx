6c3460c8718d31c8f72448f1b28aed9c3ad5a570
jjdavenport <jordanjamesdavenport@gmail.com>
2025-10-23 23:51:18 +0100

    add tablet layout

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,6 +4,7 @@ import footerImage from "../assets/461651565_2309674672719173_651412045397506519
 import headerImage from "../assets/pexels-shvetsa-5711949.jpg";
 import github from "../assets/github-142-svgrepo-com.svg";
 import linkedin from "../assets/linkedin-svgrepo-com.svg";
+import { useMediaQuery } from "react-responsive";
 
 export const Wrapper = ({ children }: { children: ReactNode }) => (
   <div className="relative">
@@ -29,7 +30,7 @@ export const Wrapper = ({ children }: { children: ReactNode }) => (
 export const Container = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className="flex flex-col gap-28 p-6">{children}</div>
+      <div className="flex flex-col gap-28 p-6 md:p-10">{children}</div>
     </>
   );
 };
@@ -49,7 +50,7 @@ export const Main = () => {
         <h2 className="font-playfair-display text-center text-[2.5rem]">
           My work
         </h2>
-        <ul className="flex flex-col gap-16">
+        <ul className="flex flex-col gap-16 md:grid md:grid-cols-2 md:grid-rows-3">
           {colors.map((i) => (
             <Project color={i} />
           ))}
@@ -60,9 +61,44 @@ export const Main = () => {
 };
 
 export const Header = () => {
+  const tablet = useMediaQuery({ minWidth: 768 });
+  if (tablet) {
+    return (
+      <>
+        <header className="relative bg-transparent">
+          <h1 className="font-playfair-display absolute top-4 right-0 z-30 text-7xl text-white">
+            Ashley Williams
+          </h1>
+
+          <div className="relative mt-28 bg-white p-10 py-20 shadow-2xl">
+            <img
+              src={headerImage}
+              alt="Ashley Williams"
+              className="float-left -mt-48 mr-6 -ml-12 w-2/4"
+            />
+
+            <h2 className="font-playfair-display mb-6 text-center text-4xl">
+              About me
+            </h2>
+
+            <p className="font-roboto text-lg leading-relaxed text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              debitis minus ad expedita, quam libero optio placeat distinctio
+              non accusantium at error ex repudiandae fugit, doloribus ab quos
+              voluptatum. Consequatur. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quo quisquam repellendus reiciendis hic quas
+              ullam eum minima vel corporis veritatis veniam, in dolorem
+              perferendis consequatur dolore! Illum fuga at aspernatur?
+            </p>
+          </div>
+        </header>
+      </>
+    );
+  }
+
   return (
     <>
-      <header className="flex flex-col gap-4">
+      <header className="relative flex flex-col gap-4">
         <div className="relative flex flex-col">
           <img src={headerImage} alt="header image" />
           <h1 className="font-playfair-display absolute bottom-0 w-full px-4 pb-2 text-left text-7xl text-white">
@@ -78,7 +114,7 @@ export const Header = () => {
 const Content = () => {
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 bg-white">
         <h2 className="font-playfair-display text-center text-4xl">About me</h2>
         <div className="flex flex-col gap-2">
           <p className="font-roboto">
@@ -153,18 +189,20 @@ const Project = ({ color }: { color: string }) => {
 export const Footer = () => {
   return (
     <>
-      <footer>
-        <div className="bg-blue flex flex-col gap-6 px-6 pt-8 pb-20">
-          <h4 className="font-playfair-display text-center text-5xl text-white">
+      <footer className="bg-blue flex flex-col md:flex-row md:p-10">
+        <div className="flex flex-col gap-6 px-6 pt-8 pb-20 md:w-2/3 md:justify-center md:px-0 md:pt-0 md:pb-0">
+          <h4 className="font-playfair-display text-center text-5xl text-white md:text-left">
             Contact me
           </h4>
-          <p className="font-roboto text-2xl text-white">
+          <p className="font-roboto text-2xl text-white md:w-96">
             Please get in touch if you think our work could be mutually
             beneficial!
           </p>
           <div className="flex flex-col">
-            <span className="font-roboto text-white">1234 Random Road</span>
-            <span className="font-roboto text-white">
+            <span className="font-roboto text-white md:text-2xl">
+              1234 Random Road
+            </span>
+            <span className="font-roboto text-white md:text-2xl">
               Random Town, California 12345
             </span>
           </div>
@@ -172,10 +210,10 @@ export const Footer = () => {
             <FooterItem element={<Phone />} text="555-555-5555" />
             <FooterItem
               element={<Mail />}
-              text="ashleywilliams.is.not.real@example.com"
+              text="ashleywilliams.is.not.real@gmail.com"
             />
           </ul>
-          <ul className="flex justify-center gap-6">
+          <ul className="flex justify-center gap-6 md:justify-start">
             <ContentLink
               element={
                 <img
@@ -197,7 +235,7 @@ export const Footer = () => {
             <ContentLink element={<Twitter className="size-10 fill-black" />} />
           </ul>
         </div>
-        <img src={footerImage} alt="footer image" />
+        <img src={footerImage} className="md:w-1/3" alt="footer image" />
       </footer>
     </>
   );
@@ -212,7 +250,7 @@ const FooterItem = ({
 }) => {
   return (
     <>
-      <li className="font-roboto flex gap-2 text-white">
+      <li className="font-roboto flex items-center gap-2 text-white md:text-2xl">
         {element}
         <span>{text}</span>
       </li>
