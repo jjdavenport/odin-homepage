fbcd95151486ce12171ea233837b0a16b949b1bf
jjdavenport <jordanjamesdavenport@gmail.com>
2025-10-26 17:02:26 +0000

    add accessibility

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -10,6 +10,7 @@ import { useMediaQuery } from "react-responsive";
 export const Wrapper = ({ children }: { children: ReactNode }) => (
   <div className="relative">
     <svg
+      aria-hidden="true"
       className="absolute top-0 left-0 w-full"
       viewBox="0 0 100 96"
       preserveAspectRatio="none"
@@ -89,21 +90,17 @@ export const Header = () => {
           <h1 className="font-playfair-display absolute top-4 right-0 z-30 text-7xl text-white">
             Ashley Williams
           </h1>
-
           <div className="relative mt-28 bg-white p-10 py-20 shadow-2xl">
-            {/* Image container */}
-            <div className="relative float-left -mt-48 mr-6 -ml-12 h-[400px] w-1/2">
+            <div className="relative float-left -mt-48 mr-5 -ml-12 h-103.75 w-1/2">
               <img
                 src={headerImageTablet}
                 alt="Ashley Williams"
                 className="absolute inset-0 h-full w-full object-cover"
               />
             </div>
-
             <h2 className="font-playfair-display mb-6 text-center text-4xl">
               About me
             </h2>
-
             <p className="font-roboto text-lg leading-relaxed text-gray-800">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
               debitis minus ad expedita, quam libero optio placeat distinctio
@@ -123,7 +120,7 @@ export const Header = () => {
     <>
       <header className="relative flex flex-col gap-4">
         <div className="relative flex flex-col">
-          <img src={headerImage} alt="header image" />
+          <img aria-hidden="true" src={headerImage} alt="header image" />
           <h1 className="font-playfair-display absolute bottom-0 w-full px-4 pb-2 text-left text-7xl text-white">
             Ashley Williams
           </h1>
@@ -153,13 +150,21 @@ const Content = () => {
           </p>
           <ul className="flex justify-center gap-4 lg:justify-end">
             <ContentLink
+              ariaLabel="github link"
               element={
-                <img className="size-10 lg:size-8" src={github} alt="github" />
+                <img
+                  className="size-10 lg:size-8"
+                  aria-hidden="true"
+                  src={github}
+                  alt="github"
+                />
               }
             />
             <ContentLink
+              ariaLabel="linkedin link"
               element={
                 <img
+                  aria-hidden="true"
                   className="size-10 lg:size-8"
                   src={linkedin}
                   alt="linkedin"
@@ -167,7 +172,13 @@ const Content = () => {
               }
             />
             <ContentLink
-              element={<Twitter className="size-10 fill-black lg:size-8" />}
+              ariaLabel="twitter link"
+              element={
+                <Twitter
+                  aria-hidden="true"
+                  className="size-10 fill-black lg:size-8"
+                />
+              }
             />
           </ul>
         </div>
@@ -176,11 +187,24 @@ const Content = () => {
   );
 };
 
-const ContentLink = ({ element }: { element: ReactNode }) => {
+const ContentLink = ({
+  element,
+  ariaLabel,
+}: {
+  element: ReactNode;
+  ariaLabel: string;
+}) => {
   return (
     <>
       <li>
-        <a href="">{element}</a>
+        <a
+          aria-label={ariaLabel}
+          className="outline-offset-4 outline-black"
+          target="_blank"
+          href=""
+        >
+          {element}
+        </a>
       </li>
     </>
   );
@@ -189,10 +213,12 @@ const ContentLink = ({ element }: { element: ReactNode }) => {
 const Project = ({ color }: { color: string }) => {
   return (
     <>
-      <div className="shadow-xl">
+      <li className="shadow-xl">
         <div>
           <div className={`${color} flex h-72 items-center justify-center p-6`}>
-            <span className="font-playfair-display text-center text-5xl text-white">
+            <span
+              className={`${color === "bg-orange" ? "text-black" : "text-white"} font-playfair-display text-center text-5xl`}
+            >
               screenshot of project
             </span>
           </div>
@@ -201,11 +227,26 @@ const Project = ({ color }: { color: string }) => {
           <div className="flex items-center justify-between">
             <span className="font-playfair-display text-2xl">Project name</span>
             <div className="flex gap-4">
-              <a href="">
-                <img className="size-6" src={github} alt="github" />
+              <a
+                aria-label="link to github"
+                className="outline-offset-4"
+                target="_blank"
+                href=""
+              >
+                <img
+                  aria-hidden="true"
+                  className="size-6"
+                  src={github}
+                  alt="github"
+                />
               </a>
-              <a href="">
-                <ExternalLink className="size-6" />
+              <a
+                aria-label="external link"
+                className="outline-offset-4"
+                target="_blank"
+                href=""
+              >
+                <ExternalLink aria-hidden="true" className="size-6" />
               </a>
             </div>
           </div>
@@ -214,7 +255,7 @@ const Project = ({ color }: { color: string }) => {
             do.
           </p>
         </div>
-      </div>
+      </li>
     </>
   );
 };
@@ -225,32 +266,37 @@ export const Footer = () => {
       <footer className="bg-blue flex w-full flex-col items-center">
         <div className="flex max-w-6xl flex-col md:flex-row md:p-10">
           <div className="flex flex-col gap-6 px-6 pt-8 pb-20 md:w-2/3 md:justify-center md:px-0 md:pt-0 md:pb-0">
-            <h4 className="font-playfair-display text-center text-5xl text-white md:text-left">
+            <h3 className="font-playfair-display text-center text-5xl text-white md:text-left">
               Contact me
-            </h4>
+            </h3>
             <p className="font-roboto text-2xl text-white md:w-96">
               Please get in touch if you think our work could be mutually
               beneficial!
             </p>
             <div className="flex flex-col">
-              <span className="font-roboto text-white md:text-2xl">
+              <span className="font-roboto md:text-2xl md:text-white">
                 1234 Random Road
               </span>
-              <span className="font-roboto text-white md:text-2xl">
+              <span className="font-roboto md:text-2xl md:text-white">
                 Random Town, California 12345
               </span>
             </div>
             <ul className="flex flex-col gap-2">
-              <FooterItem element={<Phone />} text="555-555-5555" />
               <FooterItem
-                element={<Mail />}
+                element={<Phone aria-hidden="true" />}
+                text="555-555-5555"
+              />
+              <FooterItem
+                element={<Mail aria-hidden="true" />}
                 text="ashleywilliams.is.not.real@gmail.com"
               />
             </ul>
             <ul className="flex justify-center gap-6 md:justify-start">
               <ContentLink
+                ariaLabel="github link"
                 element={
                   <img
+                    aria-hidden="true"
                     className="size-10 object-contain"
                     src={github}
                     alt="github"
@@ -258,8 +304,10 @@ export const Footer = () => {
                 }
               />
               <ContentLink
+                ariaLabel="linkedin link"
                 element={
                   <img
+                    aria-hidden="true"
                     className="size-10 object-contain"
                     src={linkedin}
                     alt="linkedin"
@@ -267,11 +315,15 @@ export const Footer = () => {
                 }
               />
               <ContentLink
-                element={<Twitter className="size-10 fill-black" />}
+                ariaLabel="twitter link"
+                element={
+                  <Twitter aria-hidden="true" className="size-10 fill-black" />
+                }
               />
             </ul>
           </div>
           <img
+            aria-hidden="true"
             loading="lazy"
             src={footerImage}
             className="md:w-1/3"
@@ -292,9 +344,9 @@ const FooterItem = ({
 }) => {
   return (
     <>
-      <li className="font-roboto flex items-center gap-2 text-white md:text-2xl">
+      <li className="font-roboto flex items-center gap-2 text-black md:text-2xl md:text-white">
         {element}
-        <span>{text}</span>
+        {text}
       </li>
     </>
   );
